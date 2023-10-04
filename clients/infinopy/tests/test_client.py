ec4842c46068882b67acbdc64b19548860a53c44
Vinay Kakade <vinaykakade@gmail.com>
2023-10-04 14:50:12 +0530

    Test for base url

--- clients/infinopy/tests/test_client.py
+++ clients/infinopy/tests/test_client.py
@@ -97,6 +97,22 @@ class InfinoClientTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.text, "index")
 
+    def test_base_url(self):
+        # Check default base url
+        client = InfinoClient()
+        assert client.get_base_url() == "http://localhost:3000"
+
+        # Check the base url when env var INFINO_BASE_URL is set
+        value = "http://SOME_ENV_URL"
+        os.environ["INFINO_BASE_URL"] = value
+        client = InfinoClient()
+        assert client.get_base_url() == value
+
+        # Check the base url when client is explicitly passed one
+        value = "http://SOME_OTHER_URL"
+        client = InfinoClient(value)
+        assert client.get_base_url() == value
+
 
 if __name__ == "__main__":
     unittest.main()
