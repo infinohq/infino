29e0d5fd94305249553b5262f0b50a88133433e0
Vinay Kakade <vinaykakade@gmail.com>
2023-10-11 20:06:10 +0530

    search as you type

--- dashboard/src/example_log_dashboard.py
+++ dashboard/src/example_log_dashboard.py
@@ -1,7 +1,7 @@
 import streamlit as st
+from st_keyup import st_keyup
 import os
 import pandas as pd
-import numpy as np
 import plotly.express as px
 
 from infinopy import InfinoClient
@@ -10,56 +10,84 @@ from util.toml_util import get_server_url
 
 
 def get_user_query():
-    user_query = st.text_input(
-        "Enter search text below and hit Enter:", "directory index forbidden"
+    """
+    Display a text input and return the user submitted text
+    """
+    user_query = st_keyup(
+        "Enter search text:", value="directory index forbidden", debounce=500, key="0"
     )
+
+    # user_query = st.text_input(
+    #    "Enter search text below and hit Enter:", "directory index forbidden"
+    # )
     return user_query
 
 
 def search_logs(infino_server_url, user_query):
+    """
+    Search infino logs and returns two dataframes:
+    - A datafrme with counts of error logs, grouped by date,
+    - A dataframe of results returned by Infino
+    """
+
+    # Search Infino for the given query
     client = InfinoClient(infino_server_url)
+    response = client.search_log(text=user_query)
+    if response.status_code != 200:
+        st.error("Could not execute search query")
+        return None, None
 
-    results = client.search_log(text=user_query)
-    print(results)
+    # Convert the response to a dataframe
+    results = response.json()
+    df = pd.DataFrame(results)
+    if df.empty:
+        return df, df
 
-    # Sample data
-    data = {
-        "Date": pd.date_range(start="2023-01-01", end="2023-01-31", freq="D"),
-        "Value": np.random.randint(1, 100, size=(31,)),
-    }
-
-    df = pd.DataFrame(data)
-    return df, results
-
-
-def display_df(df):
-    # Sidebar with slider for time window selection
-    st.sidebar.subheader("Select Time Window")
-    start_date = st.sidebar.date_input(
-        "Start Date",
-        min(df["Date"]),
-        min_value=min(df["Date"]),
-        max_value=max(df["Date"]),
-    )
-    end_date = st.sidebar.date_input(
-        "End Date",
-        max(df["Date"]),
-        min_value=min(df["Date"]),
-        max_value=max(df["Date"]),
+    # Create a dataframe of error counts by date
+    df["date_isoformat"] = df["fields"].apply(lambda x: x.get("date_isoformat"))
+    df["log_level"] = df["fields"].apply(lambda x: x.get("log_level"))
+    error_df = df[df["log_level"] == "error"]
+    error_count_df = (
+        error_df.groupby("date_isoformat").size().reset_index(name="error_count")
     )
 
-    # Filter data based on selected time window
-    filtered_data = df[
-        (df["Date"] >= pd.to_datetime(start_date))
-        & (df["Date"] <= pd.to_datetime(end_date))
-    ]
+    df["message"] = df["fields"].apply(lambda x: x.get("message"))
 
-    # Display bar chart
-    st.subheader("Bar Chart")
-    fig = px.bar(
-        filtered_data, x="Date", y="Value", title="Bar Chart for Selected Time Window"
-    )
-    st.plotly_chart(fig)
+    # Convert the numeric 'timestamp' column to datetime objects
+    df["timestamp"] = pd.to_datetime(df["time"], unit="s")
+
+    # Convert the 'timestamp' column to ISO datetime format in UTC
+    df["date"] = df["timestamp"].dt.tz_localize("UTC").dt.strftime("%Y-%m-%dT%H:%M:%SZ")
+
+    # Create a results dataframe with a subset of columns from the df
+    columns = ["date", "log_level", "message"]
+    results_df = df[columns]
+
+    return error_count_df, results_df
+
+
+def display_error_df(df):
+    """
+    Display the error dataframe as a plotly chart
+    """
+    if not df.empty:
+        fig = px.bar(
+            df,
+            x="date_isoformat",
+            y="error_count",
+            title="Errror Count by Date",
+        )
+        st.plotly_chart(fig)
+    else:
+        st.text("No error logs returned")
+
+
+def display_results_df(df, num_rows):
+    """
+    Display the results dataframe as a table
+    """
+    if not df.empty:
+        st.table(df)
 
 
 if __name__ == "__main__":
@@ -73,7 +101,12 @@ if __name__ == "__main__":
     user_query = get_user_query()
 
     # Query Infino to create a dataframe to be plotted
-    df, results = search_logs(infino_server_url, user_query)
+    error_count_df, results_df = search_logs(infino_server_url, user_query)
 
-    # Display the dataframe
-    display_df(df)
+    # Display the error graph
+    display_error_df(error_count_df)
+
+    print(results_df)
+
+    # Display results - first 100 rows
+    display_results_df(results_df, 1000)
