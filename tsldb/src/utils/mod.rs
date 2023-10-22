126c6636b8b2b2b104f1863391f665395a89718e
Vinay Kakade <vinaykakade@gmail.com>
2023-10-22 19:44:17 +0530

    Use FIELD_DELIMITER corrctly in test

--- tsldb/src/utils/mod.rs
+++ tsldb/src/utils/mod.rs
@@ -2,8 +2,8 @@ pub(crate) mod custom_serde;
 pub(crate) mod error;
 pub(crate) mod range;
 pub(crate) mod serialize;
-pub(crate) mod tokenize;
 
 pub mod config;
 pub mod io;
 pub mod sync;
+pub mod tokenize;
