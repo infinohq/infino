44d539a713849bb558989c5a9b024d2de68a1f84
Vinay Kakade <vinaykakade@gmail.com>
2023-12-10 18:49:40 +0530

    Default base url to use http on localohost, update version

--- clients/infinopy/infinopy/client.py
+++ clients/infinopy/infinopy/client.py
@@ -1,12 +1,13 @@
 import os
 import requests
 
+
 class InfinoClient:
     def __init__(self, base_url=None):
         if base_url:
             self.base_url = base_url
         else:
-            self.base_url = os.environ.get("INFINO_BASE_URL", "https://localhost:3000")
+            self.base_url = os.environ.get("INFINO_BASE_URL", "http://localhost:3000")
 
     def _request(self, method, path, params=None, json=None):
         url = self.base_url + path
@@ -24,15 +25,15 @@ class InfinoClient:
     # Deprecated. Kept here for backwards compatibility. TODO: Remove by Jan 2024.
     def append_ts(self, payload):
         return self.append_metric(payload)
-    
+
     def append_metric(self, payload):
         path = "/append_metric"
         return self._request("POST", path, json=payload)
-    
+
     # Deprecated. Kept here for backwards compatibility. TODO: Remove by Jan 2024.
     def search_log(self, text, start_time=None, end_time=None):
         return self.search_logs(text, start_time, end_time)
-    
+
     def search_logs(self, text, start_time=None, end_time=None):
         path = "/search_logs"
         params = {"text": text, "start_time": start_time, "end_time": end_time}
@@ -48,7 +49,7 @@ class InfinoClient:
     # Deprecated. Kept here for backwards compatibility. TODO: Remove by Jan 2024.
     def search_ts(self, label_name, label_value, start_time=None, end_time=None):
         return self.search_metrics(label_name, label_value, start_time, end_time)
-    
+
     def search_metrics(self, label_name, label_value, start_time=None, end_time=None):
         path = "/search_metrics"
         params = {
