85d411b04d19ec311b84c82d5948c9c67099f39a
Vinay Kakade <vinaykakade@gmail.com>
2024-01-26 20:46:14 +0530

    in progress changes

---
+++ examples/rust-apache-logs/src/main.rs
@@ -0,0 +1,82 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+use clap::{arg, Command};
+
+#[derive(Debug)]
+struct ApacheLogEntry {
+  ip: String,
+  datetime: String,
+  request: String,
+  status_code: u16,
+  response_size: usize,
+  referrer: String,
+  user_agent: String,
+}
+
+fn parse_log_line(line: &str) -> Option<ApacheLogEntry> {
+  let parts: Vec<&str> = line.split_whitespace().collect();
+  if parts.len() >= 10 {
+    let ip = parts[0].to_string();
+    let datetime =
+      parts[3].trim_start_matches('[').to_string() + " " + parts[4].trim_end_matches(']');
+    let request = parts[5].trim_start_matches('"').to_string()
+      + " "
+      + parts[6]
+      + " "
+      + parts[7].trim_end_matches('"');
+    let status_code = parts[8].parse().ok()?;
+    let response_size = parts[9].parse().ok()?;
+    let referrer = parts[10].trim_matches('"').to_string();
+    let user_agent = parts[11..].join(" ").trim_matches('"').to_string();
+
+    Some(ApacheLogEntry {
+      ip,
+      datetime,
+      request,
+      status_code,
+      response_size,
+      referrer,
+      user_agent,
+    })
+  } else {
+    None
+  }
+}
+
+fn main() {
+  // Parse command line arguments.
+  let matches = Command::new("Index Apache Logs")
+    .version("1.0")
+    .about("Index Apache Logs")
+    .arg(
+      arg!(--file <VALUE>)
+        .required(false)
+        .default_value("examples/rust-apache-logs/data/apache-tiny.log"),
+    )
+    .arg(
+      arg!(--count <VALUE>)
+        .required(false)
+        .default_value("100000")
+        .value_parser(clap::value_parser!(usize)),
+    )
+    .get_matches();
+
+  let file = matches
+    .get_one::<String>("file")
+    .expect("Could not get file");
+  let count = matches
+    .get_one::<usize>("count")
+    .expect("Could not get count");
+  println!("Indexing first {} log lines", count);
+
+  let file = File::open(file).expect("Could not open apache.log");
+  let reader = BufReader::new(file);
+
+  for line in reader.lines().take(*count) {
+    let line = line.expect("Could not receive line");
+    if let Some(log_entry) = parse_log_line(&line) {
+      println!("Indexing {:?}", log_entry);
+    }
+  }
+}
