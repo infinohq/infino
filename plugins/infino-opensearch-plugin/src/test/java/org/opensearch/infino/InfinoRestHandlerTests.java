f3b2ea45d7486188ac96a8aaa80ab9036d9fe07b
Ekechi Nwokah <ekechi.nwokah@cerbero.ai>
2024-01-30 08:43:33 -0800

    Change plugin license to Elastic

--- plugins/infino-opensearch-plugin/src/test/java/org/opensearch/infino/InfinoRestHandlerTests.java
+++ plugins/infino-opensearch-plugin/src/test/java/org/opensearch/infino/InfinoRestHandlerTests.java
@@ -1,10 +1,7 @@
-/*
- * SPDX-License-Identifier: Apache-2.0
- *
- * The OpenSearch Contributors require contributions made to
- * this file be licensed under the Apache-2.0 license or a
- * compatible open source license.
- */
+/**
+/* This code is licensed under Elastic License 2.0
+/* https://www.elastic.co/licensing/elastic-license
+/**
 
 package org.opensearch.infino;
 
