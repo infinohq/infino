7efafaf521fc95e5e65caef5f5a7f91d18e4869c
Vinay Kakade <vinaykakade@gmail.com>
2024-03-11 17:35:42 +0530

    additional test

--- coredb/src/segment_manager/wal.rs
+++ coredb/src/segment_manager/wal.rs
@@ -25,11 +25,16 @@ impl WriteAheadLog {
   }
 
   pub async fn append(&self, entry: Value) -> Result<(), CoreDBError> {
-    let mut buffer = self.buffer.lock().await;
-    buffer.push(entry);
+    let num_entries;
 
-    if buffer.len() >= MAX_ENTRIES {
-      drop(buffer); // Release the lock before flushing
+    {
+      // Write in a new scope to release buffer lock immediately.
+      let mut buffer = self.buffer.lock().await;
+      buffer.push(entry);
+      num_entries = buffer.len();
+    }
+
+    if num_entries >= MAX_ENTRIES {
       self.flush().await?;
     }
     Ok(())
@@ -61,6 +66,7 @@ mod tests {
   use std::fs;
   use std::path::Path;
   use tempfile::NamedTempFile;
+  use tokio::task;
 
   #[tokio::test]
   async fn test_write_and_flush() {
@@ -96,5 +102,58 @@ mod tests {
     assert_eq!(contents.matches('\n').count(), MAX_ENTRIES + 2); // Each entry should be on a new line
 
     fs::remove_file(path).unwrap(); // Clean up after the test
+  }
+
+  #[tokio::test]
+  async fn test_append_and_flush_parallel() {
+    let temp_file = NamedTempFile::new().unwrap();
+    let path = temp_file.path().to_str().unwrap();
+    let wal = Arc::new(WriteAheadLog::new(path).unwrap());
+
+    const NUM_APPEND_THREADS: usize = 20;
+    const NUM_FLUSH_THREADS: usize = 10;
+    const NUM_APPENDS: usize = 50000; // Number of append operations per thread.
+    const NUM_FLUSHES: usize = 10; // Number of flush operations per thread.
+
+    // Spawn tasks for concurrent appends.
+    let mut append_handles = vec![];
+    for _ in 0..NUM_APPEND_THREADS {
+      let wal_clone = Arc::clone(&wal);
+      let handle = task::spawn(async move {
+        for _ in 0..NUM_APPENDS {
+          let entry = json!({"time": 1627590000, "message": "Concurrent append"});
+          wal_clone.append(entry).await.unwrap();
+        }
+      });
+      append_handles.push(handle);
+    }
+
+    // Spawn tasks for concurrent flushes.
+    let mut flush_handles = vec![];
+    for _ in 0..NUM_FLUSH_THREADS {
+      let wal_clone = Arc::clone(&wal);
+      let handle = task::spawn(async move {
+        for _ in 0..NUM_FLUSHES {
+          wal_clone.flush().await.unwrap();
+        }
+      });
+      flush_handles.push(handle);
+    }
+
+    // Await all append and flush tasks to ensure completion.
+    for handle in append_handles {
+      handle.await.unwrap();
+    }
+    for handle in flush_handles {
+      handle.await.unwrap();
+    }
+
+    // Final flush to ensure all entries are written.
+    wal.flush().await.unwrap();
+
+    // Verify that entries were written correctly.
+    let contents = fs::read_to_string(temp_file.path()).unwrap();
+    let num_lines = contents.matches('\n').count();
+    assert!(num_lines == NUM_APPENDS * NUM_APPEND_THREADS);
   }
 }
