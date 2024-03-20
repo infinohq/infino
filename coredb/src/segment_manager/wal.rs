fbb54c5d8677a5da409f0c111a1f20d724ef30e3
Vinay Kakade <vinaykakade@gmail.com>
2024-03-20 11:15:16 +0530

    Add a function to read existing write ahead log (#304)

--- coredb/src/segment_manager/wal.rs
+++ coredb/src/segment_manager/wal.rs
@@ -1,5 +1,5 @@
-use std::fs::{metadata, remove_file, OpenOptions};
-use std::io::{BufWriter, Write};
+use std::fs::{metadata, remove_file, File, OpenOptions};
+use std::io::{BufRead, BufReader, BufWriter, Write};
 
 use serde_json::Value;
 
@@ -7,14 +7,21 @@ use crate::utils::error::CoreDBError;
 
 const MAX_ENTRIES: usize = 1000;
 
+/// File based write ahead log. Typically, a write ahead log is created for each segment.
 #[derive(Debug)]
 pub struct WriteAheadLog {
+  /// Path to the write ahead log file.
   file_path: String,
+
+  /// Temporary memory buffer to store write ahead log entries.
   buffer: Vec<Value>,
+
+  /// Write ahead log file writer.
   writer: BufWriter<std::fs::File>,
 }
 
 impl WriteAheadLog {
+  /// Create a new write ahead log.
   pub fn new(path: &str) -> Result<Self, CoreDBError> {
     let file = OpenOptions::new().create(true).append(true).open(path)?;
     let writer = BufWriter::new(file);
@@ -27,6 +34,34 @@ impl WriteAheadLog {
     })
   }
 
+  /// Read all entries from the write ahead log.
+  pub fn read_all(&self) -> Result<Vec<Value>, CoreDBError> {
+    // Create buffered reader to read the file line by line.
+    let mut json_values = Vec::new();
+    let file = File::open(&self.file_path)?;
+    let reader = BufReader::new(file);
+
+    // Read each line and parse it as JSON. Ignore lines that cannot be read or parsed as JSON.
+    // This could happen in case of a crash/partial data received. We ignore it to get to last known good state.
+    for line in reader.lines() {
+      let line = match line {
+        Ok(line) => line,
+        Err(_) => continue, // Ignore lines that cannot be read.
+      };
+
+      let json_value = match serde_json::from_str(&line) {
+        Ok(value) => value,
+        Err(_) => continue, // Ignore lines that cannot be parsed as JSON.
+      };
+
+      json_values.push(json_value);
+    }
+
+    Ok(json_values)
+  }
+
+  /// Append an entry to the write ahead log. After appending MAX_ENTRIES entries, all the entries
+  /// will be flushed to disk.
   pub fn append(&mut self, entry: Value) -> Result<(), CoreDBError> {
     self.buffer.push(entry);
 
@@ -36,6 +71,7 @@ impl WriteAheadLog {
     Ok(())
   }
 
+  /// Flush write ahead log to disk.
   pub fn flush(&mut self) -> Result<(), CoreDBError> {
     if !self.buffer.is_empty() {
       let combined_entries = self
@@ -52,6 +88,7 @@ impl WriteAheadLog {
     Ok(())
   }
 
+  /// Delete the write ahead log - typically called when after a segment is committed.
   pub fn remove(&mut self) -> Result<(), CoreDBError> {
     // Delete the file - if it exists.
     if metadata(&self.file_path).is_ok() {
@@ -104,12 +141,16 @@ mod tests {
 
     // Flush should be called automatically as we reach MAX_ENTRIES limit.
 
-    // Read back the log file. Should noe contain 'Test log entry' MAX_ENTRIES+2 times.
-    let contents = fs::read_to_string(Path::new(path)).unwrap();
-    assert!(contents.contains("Test log entry"));
-    assert_eq!(contents.matches('\n').count(), MAX_ENTRIES + 2); // Each entry should be on a new line
+    // Read back the log file. Should now contain 'Test log entry' MAX_ENTRIES+2 times.
+    let wal = WriteAheadLog::new(path).unwrap();
+    let contents = wal.read_all().unwrap();
+    assert_eq!(contents.len(), MAX_ENTRIES + 2);
+    for content in contents {
+      assert_eq!(content, entry);
+    }
 
     // Remove the file and check that it does not exist anymore.
+    let mut wal = wal;
     wal.remove().unwrap();
     assert!(std::fs::metadata(path).is_err())
   }
@@ -123,18 +164,18 @@ mod tests {
 
     const NUM_APPEND_THREADS: usize = 20;
     const NUM_FLUSH_THREADS: usize = 10;
-    const NUM_APPENDS: usize = 50000; // Number of append operations per thread.
-    const NUM_FLUSHES: usize = 10; // Number of flush operations per thread.
+    const NUM_APPENDS_PER_THREAD: usize = 50000; // Number of append operations per thread.
+    const NUM_FLUSHES_PER_THREAD: usize = 10; // Number of flush operations per thread.
 
     // Spawn tasks for concurrent appends.
     let mut append_handles = vec![];
     for _ in 0..NUM_APPEND_THREADS {
       let wal_clone = Arc::clone(&wal);
       let handle = task::spawn(async move {
-        for _ in 0..NUM_APPENDS {
+        for _ in 0..NUM_APPENDS_PER_THREAD {
           let entry = json!({"time": 1627590000, "message": "Concurrent append"});
           let wal_clone = &mut wal_clone.lock().await;
-          wal_clone.append(entry).unwrap();
+          wal_clone.append(entry.clone()).unwrap();
         }
       });
       append_handles.push(handle);
@@ -145,7 +186,7 @@ mod tests {
     for _ in 0..NUM_FLUSH_THREADS {
       let wal_clone = wal.clone();
       let handle = task::spawn(async move {
-        for _ in 0..NUM_FLUSHES {
+        for _ in 0..NUM_FLUSHES_PER_THREAD {
           let wal_clone = &mut wal_clone.lock().await;
           wal_clone.flush().unwrap();
         }
@@ -166,9 +207,15 @@ mod tests {
     let wal_clone = &mut wal_clone.lock().await;
     wal_clone.flush().unwrap();
 
-    // Verify that entries were written correctly.
-    let contents = fs::read_to_string(temp_file.path()).unwrap();
-    let num_lines = contents.matches('\n').count();
-    assert!(num_lines == NUM_APPENDS * NUM_APPEND_THREADS);
+    // Read back the log file. Should now contain 'Test log entry' MAX_ENTRIES+2 times.
+    let wal = WriteAheadLog::new(path).unwrap();
+    let contents = wal.read_all().unwrap();
+    assert_eq!(contents.len(), NUM_APPENDS_PER_THREAD * NUM_APPEND_THREADS);
+    for content in contents {
+      assert_eq!(
+        content,
+        json!({"time": 1627590000, "message": "Concurrent append"})
+      );
+    }
   }
 }
