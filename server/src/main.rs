c9cd6c566e1375dc7a983ea40a0b5b735ad78b29
Ekechi Nwokah <ekechi.nwokah@cerbero.ai>
2024-03-22 16:41:00 -0700

    Fix bug with bulk requests

--- server/src/main.rs
+++ server/src/main.rs
@@ -567,9 +567,19 @@ async fn append_metric(
   Ok(())
 }
 
+// TODO: This feels like a lot of string processing on each bulk request. Simplify this.
 fn process_bulk_json_body(json_body: &str) -> Result<Vec<Value>, (StatusCode, String)> {
   // Preprocess the input to ensure it's in a JSON array format
-  let json_array_string = format!("{}", json_body.replace("\n", ",").trim_matches(','));
+  let json_array_string = json_body
+    .trim() // Trim leading/trailing whitespace
+    .replace("\r\n", "\n") // Replace \r\n with \n
+    .split('\n') // Split by newline character
+    .filter(|s| !s.trim().is_empty()) // Filter out empty lines
+    .map(|s| s.trim().to_string()) // Trim whitespace from each line
+    .collect::<Vec<String>>()
+    .join(","); // Join the objects with commas
+
+  let json_array_string = format!("[{}]", json_array_string); // Wrap the string in square brackets
 
   // Parse the preprocessed string as a JSON array
   match serde_json::from_str::<Vec<Value>>(&json_array_string) {
@@ -584,7 +594,7 @@ fn process_bulk_json_body(json_body: &str) -> Result<Vec<Value>, (StatusCode, St
       error!("Failed to parse JSON array: {}", e);
       Err((
         StatusCode::BAD_REQUEST,
-        "Failed to parse JSON array from input.".to_string(),
+        format!("Failed to parse JSON array from input: {}", e),
       ))
     }
   }
